c5a512f2b5dc46d2b7dc317cf2d73991aff69be2
Augusto Ojeda <okarinex@gmail.com>
2021-12-22 23:06:18 -0300

    complentando el desafio 2/3

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Item from './Item'
+import { useState, useEffect } from 'react';
 
 
 
@@ -10,49 +11,64 @@ import Item from './Item'
 
 const ItemList = () => {
 
-    const delay = new Promise ((res,rej)=>{
-        const modSamsung = [
+    const modSamsung = [
 
-            {
-                id: 1,
-                src: '',
-                title: 'Galaxy S21',
-                memoria: 128,
-                ram: 8,
-                camara: 12,
-                bateria: 4000,
-                precio: 120000,
-            },
-            {
-                id: 2,
-                src: '',
-                title: 'Z-Flip 3',
-                memoria: 128,
-                ram: 8,
-                camara: 12,
-                bateria: 4000,
-                precio: 130000
-            }
-        ];
+        {
+            id: 1,
+            src: '',
+            title: 'Galaxy S21',
+            memoria: 128,
+            ram: 8,
+            camara: 12,
+            bateria: 4000,
+            precio: 120000,
+        },
+        {
+            id: 2,
+            src: '',
+            title: 'Z-Flip 3',
+            memoria: 128,
+            ram: 8,
+            camara: 12,
+            bateria: 4000,
+            precio: 130000
+        }
+    ];
 
-        setTimeout(() => {
-            if(modSamsung.length > 0){
-                res(modSamsung.map( m => < Item key={m.id} modelo={m.title} bateria={m.bateria} memoria={m.memoria}  precio={m.precio} img={m.src}/> ))
-            }else{
-                rej(console.log('no hay productos'))
-            }
-        }, 2000);
-    })
+    const [Items, setItems] = useState([])
 
-    delay
-        .then( res => {console.log(res)})
-        .catch( err => {console.log(err)})
+
+    const getpromesa = () =>{
+        
+        const delay = new Promise ((res,rej)=>{
+
+
+            setTimeout(() => {
+                if(modSamsung.length > 0){
+                    res( modSamsung)
+                }else{
+                    rej(console.log('no hay productos'))
+                }
+            }, 2000);
+        })
+    
+        delay
+            .then( res => {setItems(res)})
+            .catch( err =>{console.log(err)})
+
+    }
+
+    useEffect(() => {
+        getpromesa()
+    }, [])
+    
 
         
 
     return (
         <div className='flex'>
-            {  } 
+            
+            { Items.map( m => < Item key={m.id} modelo={m.title} bateria={m.bateria} memoria={m.memoria}  precio={m.precio}/> ) /* { modSamsung.map( m => < Item key={m.id} modelo={m.title} bateria={m.bateria} memoria={m.memoria}  precio={m.precio} img={m.src}/> ) }  */}
         </div>
     )
 }
